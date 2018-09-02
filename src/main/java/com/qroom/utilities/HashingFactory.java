ae15731f00698d55442d62ed679d0a01f130edc5
kudrya <kam123ua@gmail.com>
2018-09-02 15:37:28 +0300

    Solve static problem

--- src/main/java/com/qroom/utilities/HashingFactory.java
+++ src/main/java/com/qroom/utilities/HashingFactory.java
@@ -3,7 +3,7 @@ package com.qroom.utilities;
 import org.apache.commons.codec.digest.DigestUtils;
 
 public class HashingFactory {
-    public Hashing getHashing(String str) {
+    public static Hashing getHashing(String str) {
         return () -> DigestUtils.sha256Hex(str);
     }
 }
