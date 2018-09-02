09cf5fe697c2fe7f5ec0aab6c7509eb0ee44fb44
kudrya <kam123ua@gmail.com>
2018-09-02 18:32:17 +0300

    solve problem

--- src/main/java/com/qroom/controllers/FileController.java
+++ src/main/java/com/qroom/controllers/FileController.java
@@ -2,27 +2,43 @@ package com.qroom.controllers;
 
 import com.qroom.controllers.answers.Answer;
 import com.qroom.controllers.answers.ErrorAnswer;
+import com.qroom.controllers.answers.SuccessAnswer;
 import com.qroom.controllers.answers.templates.ActionServer;
+import com.qroom.controllers.answers.templates.AuthorizationTemplate;
+import com.qroom.dao.DAOFile;
+import com.qroom.dao.DAOLogin;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
+
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @RestController
 public class FileController {
+
+    @Autowired
+    private DAOFile daoFile;
+
+    @Autowired
+    private DAOLogin daoLogin;
+
     @RequestMapping(value = "/uploadFile", consumes = "multipart/form-data", headers = "content-type=multipart/*")
-    public Answer uploadFile(@RequestParam("file") MultipartFile file) {
+    public Answer uploadFile(@RequestParam("file") MultipartFile file, HttpSession session) {
         final String command = "uploadFile";
         ActionServer actionServer = () -> {
             if (file.isEmpty()) {
                 return new ErrorAnswer(command, "You failed to upload file because the file was empty.");
             } else {
-                return null;
+                try {
+                    String name = daoFile.save(file);
+                    return new SuccessAnswer<>(command, "File was uploaded successful", name);
+                } catch (IOException e) {
+                    return new ErrorAnswer(command, "Error in writing file");
+                }
             }
         };
-        return null;
+        return new AuthorizationTemplate(actionServer, session, command, daoLogin).answer();
 
     }
 }
