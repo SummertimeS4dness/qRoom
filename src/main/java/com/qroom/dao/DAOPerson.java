001f8cbb4b54e84bf79bb8dc9020ef28cd2df12e
kudrya <kam123ua@gmail.com>
2018-09-07 14:45:58 +0300

    Change dao person

--- src/main/java/com/qroom/dao/DAOPerson.java
+++ src/main/java/com/qroom/dao/DAOPerson.java
@@ -4,4 +4,8 @@ import com.qroom.dao.entities.Person;
 
 public interface DAOPerson {
     Person getPersonByLogin(String login);
+    boolean setName(long id, String name);
+    boolean setSurname(long id, String surname);
+    boolean setIcon(long id, long icon);
+    boolean setPhone(long id, String phone);
 }
